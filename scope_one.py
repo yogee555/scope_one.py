2789de3e2fc241d7cccfb2edc15bbcda2fe25d9c
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-01-13 12:19:29 +0530

    Add files via upload

---
+++ scope_one.py
@@ -0,0 +1,11 @@
+pqr = 800
+_qwer = 900
+class Besanth():
+    __x=100
+    _y=200
+    x=500
+    def greet (self):
+        print("hi i am from beasenth class")
+    def show_beasanth(self):
+        print(self.__x)
+print(Besanth.x)
